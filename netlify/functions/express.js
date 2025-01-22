4607d741dfac98aa2187c373529ef752a43aa1e1
omransoliman <omransoliman.pv@gmail.com>
2025-01-22 13:38:42 +0100

    V10

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -1,10 +1,14 @@
 // netlify/functions/express.js
 const express = require('express');
-const serverless = require('serverless-http');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Netlify!');
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from Express on Netlify!' });
 });
 
-module.exports.handler = serverless(app);
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Express function works!' })
+  };
+};
