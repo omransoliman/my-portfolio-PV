1782f0250e2b9dfc745ac486eeaae68e692ddcde
omransoliman <omransoliman.pv@gmail.com>
2025-01-22 15:07:53 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import React, { useEffect } from "react";
+
 function HomePage() {
-    return <h1>Welcome to My Portfolio</h1>;
-  }
-  
-  export default HomePage;
+  useEffect(() => {
+    // Check if the script is already added
+    if (!window.picflow) {
+      window.picflow = true;
+      const script = document.createElement("script");
+      script.src = "https://picflow.com/embed/main.js";
+      script.type = "module";
+      script.defer = true;
+      document.head.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the homepage with a Picflow gallery:</p>
+      {/* Picflow gallery element */}
+      <picflow-gallery id="gal_cBCgnwN8J3xVUCf7" lightbox="#000000E6"></picflow-gallery>
+    </div>
+  );
+}
+
+export default HomePage;
