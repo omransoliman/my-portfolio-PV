4b25e0c78ef3bd8119c34476159c13b6457e20ca
omransoliman <omransoliman.pv@gmail.com>
2025-01-22 15:29:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function HomePage() {
-  useEffect(() => {
-    // Check if the script is already added
-    if (!window.picflow) {
-      window.picflow = true;
-      const script = document.createElement("script");
-      script.src = "https://picflow.com/embed/main.js";
-      script.type = "module";
-      script.defer = true;
-      document.head.appendChild(script);
-    }
-  }, []);
-
   return (
     <div>
       <h1>Welcome to My Website</h1>
-      <p>This is the homepage with a Picflow gallery:</p>
-      {/* Picflow gallery element with new attributes */}
-      <picflow-gallery 
-        id="gal_cBCgnwN8J3xVUCf7" 
-        lightbox="#000000E6" 
-        no-padding="true" 
-        no-background="true" 
-        show-cover="true"
-      ></picflow-gallery>
+      <p>Explore my portrait gallery below:</p>
+      <iframe
+        src="https://omransoliman.pixieset.com/portrait/"
+        style={{
+          width: "100%",
+          height: "80vh",
+          border: "none",
+        }}
+        title="Portrait Gallery"
+      ></iframe>
     </div>
   );
 }
