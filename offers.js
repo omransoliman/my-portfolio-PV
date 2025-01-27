471679be92a8c8e87bac5e32bcfba24b1ab274a2
omransoliman <omransoliman.pv@gmail.com>
2025-01-27 10:45:27 +0100

    Update offers.js

--- offers.js
+++ offers.js
@@ -3,29 +3,41 @@
 // Set this variable to true or false to control the visibility of offers
 const showOffers = false; // Change to `true` to show offers, `false` to hide them
 
+// Configuration object to control the visibility of each offer
+const offerVisibility = {
+    offer1: true,  // Set to `true` to show Offer 1, `false` to hide it
+    offer2: false, // Set to `true` to show Offer 2, `false` to hide it
+    offer3: true   // Set to `true` to show Offer 3, `false` to hide it
+};
+
 // Function to initialize the offers section
 function initializeOffers() {
     const offerCards = document.querySelectorAll('.offer-card');
     const noOffersMessage = document.createElement('div');
-noOffersMessage.id = 'no-offers-message';
-noOffersMessage.innerHTML = `
-    <p>Thank you for your interest in our services! We're always looking for ways to provide exceptional value to our customers. While we don't have any special offers available at this time, we encourage you to follow us on Instagram to be the first to know about future promotions and exclusive deals.</p>
-    <a href="https://www.instagram.com/soliman.omran" target="_blank" class="btn btn--primary">Follow us on Instagram</a>
-`;
+    noOffersMessage.id = 'no-offers-message';
+    noOffersMessage.innerHTML = `
+        <p>Thank you for your interest in our services! We're always looking for ways to provide exceptional value to our customers. While we don't have any special offers available at this time, we encourage you to follow us on Instagram to be the first to know about future promotions and exclusive deals.</p>
+        <a href="https://www.instagram.com/soliman.omran" target="_blank" class="btn btn--primary">Follow us on Instagram</a>
+    `;
 
     // Insert the message at the top of the <main> container (right below the nav)
     const mainContainer = document.querySelector('main.exclusive-offer');
     mainContainer.insertBefore(noOffersMessage, mainContainer.firstChild);
 
-    // Show or hide offers based on the `showOffers` variable
+    // Show or hide offers based on the `showOffers` variable and the offerVisibility configuration
     if (showOffers) {
-        // Show offers
-        offerCards.forEach(card => {
-            card.style.display = 'flex';
+        // Show or hide each offer based on the offerVisibility configuration
+        offerCards.forEach((card, index) => {
+            const offerKey = `offer${index + 1}`; // offer1, offer2, offer3
+            if (offerVisibility[offerKey]) {
+                card.style.display = 'flex'; // Show the offer
+            } else {
+                card.style.display = 'none'; // Hide the offer
+            }
         });
-        noOffersMessage.style.display = 'none'; // Hide the message
+        noOffersMessage.style.display = 'none'; // Hide the message if any offer is visible
     } else {
-        // Hide offers
+        // Hide all offers if showOffers is false
         offerCards.forEach(card => {
             card.style.display = 'none';
         });
